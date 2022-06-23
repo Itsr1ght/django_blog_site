64d46ad24cacf39c9e53402d2ed749e0a843ddd2
Bilal <bjlegend007@gmail.com>
2022-06-23 21:16:10 +0530

    updated home page

--- article/views.py
+++ article/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import Article
 
 def article_list(request):
-    articles = Article.objects.all().order_by('date')
+    articles = Article.objects.all().order_by('-date')
     return render(request, "article_list.html", { 'articles': articles})
 
 def article_page(request , slug):
--- blog_website/views.py
+++ blog_website/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from article.models import Article
 
 def home_page(request):
-    return render(request,'home.html')
+    article = Article.objects.all().order_by('-date')[0]
+    return render(request,'home.html',{ "article" : article })
 
 
 def about_page(request):
