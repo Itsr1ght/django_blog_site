628b2caeb7e384fca2d145c63c1414e5f30461ff
Bilal <bjlegend007@gmail.com>
2025-11-11 08:50:38 +0530

    updated the MEDIA_URL

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -121,16 +121,16 @@ USE_TZ = True
 
 REPO_NAME = "django_blog_site"
 
-MEDIA_URL = '/media/'
-
 if os.getenv("GITHUB_ACTIONS"):
     BASE_URL = f'/{REPO_NAME}'
     STATIC_URL = f'/{REPO_NAME}/static/'
     DISTILL_URL_BASE = f"/{REPO_NAME}/"
+    MEDIA_URL = '/{REPO_NAME}/media/'
 else:
     BASE_URL = '/'
     STATIC_URL = 'static/'
     DISTILL_URL_BASE = "/"
+    MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static'),
